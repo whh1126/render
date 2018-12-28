8cf6d276af777c17fe9a17a309cb489c1088fe5e
vk <2389796069@qq.com>
2018-12-28 19:47:19 +0800

    添加

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,11 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
-console.log(gulp);
+gulp.task('sass', function() {
+    return gulp.src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+})
+gulp.watch('watch', function() {
+
+
+})
